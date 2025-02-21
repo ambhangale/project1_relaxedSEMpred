514b97af577736a8ad66287c3562a526dfd93081
ambhangale <148333008+ambhangale@users.noreply.github.com>
2025-02-21 14:04:15 +0100

    compare  with my function

--- RDA_SEM/testfunc_RDA_SEM.R
+++ RDA_SEM/testfunc_RDA_SEM.R
@@ -9,10 +9,11 @@
 
 source("func_RDA_SEM.R")
 
+dat <- gendat(ntrain = 250, ntest = 250, misspecify = F) # for comparison with `lavaan()` and `lm()`
+
 ## compare with `lavPredictY()` function to see if `alpha1/2 = 0` with `XYtype = "Sigma.xy"` is equivalent----
 
 ### `lavPredictY()`
-dat <- gendat(ntrain = 250, ntest = 250, misspecify = F)
 
 mod <- ' 
     # latent variable definitions
@@ -42,11 +43,39 @@ Ypred.A1 <- testrule(ntrain = 250, ntest = 250, misspecify = F, regXY = F,
 Ypred.A2 <- testrule(ntrain = 250, ntest = 250, misspecify = F, regXY = T, 
                      alpha1 = 0, alpha2 = 0)
 
-isFALSE(any(round(Ypred.lav,100) == round(Ypred.A1$Ypred,100)))
-isFALSE(any(round(Ypred.lav,100) == round(Ypred.A2$Ypred,100)))
-isFALSE(any(round(Ypred.A1$Ypred,100) == round(Ypred.A2$Ypred,100)))
+round(Ypred.lav,9) == round(Ypred.A1$Ypred,9)
+round(Ypred.lav,9) == round(Ypred.A2$Ypred,9)
+round(Ypred.A1$Ypred,9) == round(Ypred.A2$Ypred,9)
 # the three are comparable
 # rounding only to compare, because R returns FALSE due to rounding error at some nth term 
 
 ##----
 
+## compare with OLS regression to see if `alpha1/2 = 1` with `XYtype = "S.xy"` is equivalent----
+
+lmPred <- function(train, test, 
+                   xnames = c(paste0("x", 1:3), paste0("y", 1:4)), 
+                   ynames = paste0("y", 5:8)) {
+  Ypred <- list()
+  for (y in ynames) {
+    lmfit <- lm(formula(paste0(y, "~", paste0(xnames, collapse = "+"))), data = train)
+    Ypred[[match(y, ynames)]] <- predict.lm(lmfit, newdata = test)
+  }
+  Ypred.df <- do.call("cbind", Ypred)
+  colnames(Ypred.df) <- ynames
+  return(Ypred.df)
+}
+
+Ypred.lm <- lmPred(train = dat$train, test = dat$test)
+
+Ypred.A3 <- testrule(ntrain = 250, ntest = 250, misspecify = F, regXY = F, 
+                     XYtype = "S.xy", alpha1 = 1)
+Ypred.A4 <- testrule(ntrain = 250, ntest = 250, misspecify = F, regXY = T, 
+                     alpha1 = 1, alpha = 1)
+
+round(Ypred.lm,10) == round(Ypred.A3$Ypred,10)
+round(Ypred.lm,10) == round(Ypred.A4$Ypred,10)
+round(Ypred.A3$Ypred,10) == round(Ypred.A4$Ypred,10)
+# the three are comparable
+
+#----
