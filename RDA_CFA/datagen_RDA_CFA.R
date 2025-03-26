26599d37d4efd14c01b724416d891d11c1f2985e
ambhangale <148333008+ambhangale@users.noreply.github.com>
2025-03-26 15:58:14 +0100

    swap mod1 and 2 for misspecification

--- RDA_CFA/datagen_RDA_CFA.R
+++ RDA_CFA/datagen_RDA_CFA.R
@@ -1,5 +1,5 @@
 ## Aditi M. Bhangale
-## Last updated: 24 March 2025
+## Last updated: 26 March 2025
 
 # Creating a function that applies the RDA-like constraints on the SEM prediction rule
 ## CFA example
@@ -12,6 +12,35 @@ library(mvtnorm)
 library(lavaan)
 
 mod1 <- '
+# factor loadings
+F1 =~ 0.67*x1 + 0.71*x2 + 0.58*x3 + 0.83*x4 + 0.64*x5 + 0.55*x6 + 0.68*x7
+
+# factor variance
+F1 ~~ 1*F1
+
+# item (co)variances
+x1 ~~ 0.22*x1
+x2 ~~ 0.41*x2
+x3 ~~ 0.55*x3
+x4 ~~ 0.49*x4
+x5 ~~ 0.71*x5
+x6 ~~ 0.39*x6
+x7 ~~ 0.61*x7
+
+# factor mean
+F1 ~ 0*1
+
+# item means
+x1 ~ 0.91*1
+x2 ~ 0.43*1
+x3 ~ 0.73*1
+x4 ~ 0.44*1
+x5 ~ 0.83*1
+x6 ~ 0.66*1
+x7 ~ 0.54*1
+'
+
+mod2 <- '
 # factor loadings
 F1 =~ 0.67*x1 + 0.71*x2 + 0.58*x3
 F2 =~ 0.83*x4 + 0.64*x5 + 0.55*x6 + 0.68*x7
@@ -32,35 +61,6 @@ x7 ~~ 0.61*x7
 # factor means
 F1 ~ 0*1
 F2 ~ 0*1
-
-# item means
-x1 ~ 0.91*1
-x2 ~ 0.43*1
-x3 ~ 0.73*1
-x4 ~ 0.44*1
-x5 ~ 0.83*1
-x6 ~ 0.66*1
-x7 ~ 0.54*1
-'
-
-mod2 <- '
-# factor loadings
-F1 =~ 0.67*x1 + 0.71*x2 + 0.58*x3 + 0.83*x4 + 0.64*x5 + 0.55*x6 + 0.68*x7
-
-# factor variance
-F1 ~~ 1*F1
-
-# item (co)variances
-x1 ~~ 0.22*x1
-x2 ~~ 0.41*x2
-x3 ~~ 0.55*x3
-x4 ~~ 0.49*x4
-x5 ~~ 0.71*x5
-x6 ~~ 0.39*x6
-x7 ~~ 0.61*x7
-
-# factor mean
-F1 ~ 0*1
 
 # item means
 x1 ~ 0.91*1
