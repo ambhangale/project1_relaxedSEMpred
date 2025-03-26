f2ba44abbbb9cd11754db912c51713f2077e2755
ambhangale <148333008+ambhangale@users.noreply.github.com>
2025-03-26 15:59:37 +0100

    update model in fitmod to one-factor model

--- RDA_CFA/func_RDA_CFA.R
+++ RDA_CFA/func_RDA_CFA.R
@@ -1,5 +1,5 @@
 ## Aditi M. Bhangale
-## Last updated: 24 March 2025
+## Last updated: 26 March 2025
 
 # Creating a function that applies the RDA-like constraints on the SEM prediction rule
 ## CFA example
@@ -14,12 +14,10 @@ source(here("RDA_CFA", "datagen_RDA_CFA.R"))
 fitmod <- function(train) {
   mod <- '
 # factor loadings
-F1 =~ x1 + x2 + x3
-F2 =~ x4 + x5 + x6 + x7
+F1 =~ x1 + x2 + x3 + x4 + x5 + x6 + x7
 
-# factor (co)variances
-F1 ~~ 1*F1 + F2
-F2 ~~ 1*F2
+# factor variance
+F1 ~~ 1*F1
 
 # item (co)variances
 x1 ~~ x1
@@ -30,9 +28,8 @@ x5 ~~ x5
 x6 ~~ x6
 x7 ~~ x7
 
-# factor means
+# factor mean
 F1 ~ 0*1
-F2 ~ 0*1
 
 # item means
 x1 ~ 1
