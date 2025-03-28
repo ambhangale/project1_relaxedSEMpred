ca200f8b2f3ed220ea4aec8bfea1fab26d5e3a3a
ambhangale <148333008+ambhangale@users.noreply.github.com>
2025-03-28 16:59:59 +0100

    mini simulations in cross-validation framework

---
+++ RDA_CFA/cvminisims_RDA_CFA.R
@@ -0,0 +1,13 @@
+## Aditi M. Bhangale
+## Last updated: 28 March 2025
+
+# Creating a function that applies the RDA-like constraints on the SEM prediction rule
+## CFA example
+### mini simulation testing prediction rule in cross-validation setting
+
+# getwd()
+# setwd("/Users/Aditi_2/Desktop/Universiteit Leiden/Projects/project_1_relaxedSEMpred/RDA_CFA")
+
+library(here)
+source(here("RDA_CFA", "cv_RDA_CFA.R"))
+
