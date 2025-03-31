b6e0c05667388368e4865a8ffb547195564b792c
ambhangale <148333008+ambhangale@users.noreply.github.com>
2025-03-31 15:34:56 +0200

    run a simulation with 100 replications

--- RDA_CFA/cvminisims_RDA_CFA.R
+++ RDA_CFA/cvminisims_RDA_CFA.R
@@ -1,5 +1,5 @@
 ## Aditi M. Bhangale
-## Last updated: 28 March 2025
+## Last updated: 31 March 2025
 
 # Creating a function that applies the RDA-like constraints on the SEM prediction rule
 ## CFA example
@@ -10,4 +10,21 @@
 
 library(here)
 source(here("RDA_CFA", "cv_RDA_CFA.R"))
+
+# CL <- makePSOCKcluster(detectCores() - 1L)
+
+conds <- expand.grid(sampID = 1:100, nCal = c(250, 1e3, 1e4), 
+                     misspecify = c(F, T))
+
+t0 <- Sys.time()
+resList <- mcmapply(FUN = predict.y.cv, sampID = conds$sampID,
+                    nCal = conds$nCal, nPred = 1e4,
+                    misspecify = conds$misspecify, SIMPLIFY = F, 
+                    mc.cores = detectCores() - 1L) # parallelised version of mapply (parallel package)
+t1 <- Sys.time()
+diff <- difftime(t1, t0, "sec")
+
+# resList <- mapply(FUN = predict.y.cv, sampID = conds$sampID,
+#                   nCal = conds$nCal, nPred = 1e4,
+#                   misspecify = conds$misspecify, SIMPLIFY = F) # not parallelised
 
