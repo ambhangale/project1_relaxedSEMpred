5d6ea4b79bfafa7a6a720356175a2fe6c96bb18c
ambhangale <148333008+ambhangale@users.noreply.github.com>
2025-04-14 13:02:27 +0200

    add JDK fixes, improvements, and comments

--- RDA_CFA/cv_RDA_CFA.R
+++ RDA_CFA/cv_RDA_CFA.R
@@ -132,7 +132,7 @@ predict.y.part <- function(dat, K, nK,
   partdat <- partition(dat = dat, K = K, nK = nK, seed = seed) # partitioned data
   
   # row and column names
-  # FIXME JDK: would abandon this in favour of array, see previous comment
+  # FIXME JDK: would abandon this in favour of array, see previous comment 
   mat.rows <- do.call("c", lapply(1:K, 
                                   function(k) apply(expand.grid(k, 1:nK), 1, 
                                                     paste0, collapse = ".")))
@@ -140,8 +140,8 @@ predict.y.part <- function(dat, K, nK,
                     1, paste0, collapse = ",") 
   # use as.character() above to paste only 0/1 instead of 0.0 and 1.0
   # because in the for loops, a1/a2 are 0/1 not 0.0/0.1
-  # FIXME JDK: below, K*nL will not always work (when n is not divisible by K)
-  # FIXME JDK: I would recommend using an array instead of dim (n, length(alpha1), length(alpha2), length(ynames))
+  # FIXME JDK: below, K*nL will not always work (when n is not divisible by K) 
+  # FIXME JDK: I would recommend using an array instead of dim (n, length(alpha1), length(alpha2), length(ynames)) 
   sqdevmat <- matrix(NA, K*nK, length(alpha1)*length(alpha2)*length(ynames),
                     dimnames = list(mat.rows, mat.cols)) # matrix with squared deviations
  
