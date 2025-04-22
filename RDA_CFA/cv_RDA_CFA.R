2761b96a347dba47f4247591221be16976109337
ambhangale <148333008+ambhangale@users.noreply.github.com>
2025-04-22 11:33:10 +0200

    function to assign partition IDs per observation

--- RDA_CFA/cv_RDA_CFA.R
+++ RDA_CFA/cv_RDA_CFA.R
@@ -1,5 +1,5 @@
 ## Aditi M. Bhangale
-## Last updated: 14 April 2025
+## Last updated: 22 April 2025
 ## Fixes and improvements: 4 April 2025 (Julian D. Karch)
 
 # Creating a function that applies the RDA-like constraints on the SEM prediction rule
@@ -15,6 +15,34 @@ source(here("RDA_CFA", "datagen_RDA_CFA.R"))
 
 # dat <- gendat(sampID = 2, nCal = 250, nPred = 250, misspecify = F, seed = 10824) # dummy data for now
 # calibration <- dat$calibration; prediction <- dat$prediction
+
+# function to assign partition IDs per observation----
+partidx <- function(ndat, sampID = NULL, K, nK, seed = NULL) {
+  if (!is.null(sampID)) {
+    # set seed based on `sampID`
+    setSeeds(projSeeds = allSeeds, run = sampID)
+    useStream(2) # use the second stream of the `sampID` rep for partition creation
+  } else {
+    if (!is.null(seed)) {
+      set.seed(seed)
+    } else {
+      stop("specify `seed` argument if `sampID` is left blank") 
+    }
+  }
+  
+  partvec <- rep(1:K, nK)
+  
+  # randomly assign observations to one of the K parts such that there are nK 
+  # observations per part
+  part.id <- sample(x = partvec, size = ndat, replace = F)
+  
+  return(part.id)
+  
+}
+
+# partidx(ndat = 100, sampID = 2, K = 10, nK = 10)
+
+#----
 
 # function to partition data into K parts----
 partition <- function(dat, K, nK, seed) { # input here is the calibration data
@@ -402,6 +430,6 @@ predict.y.en <- function(sampID, nCal, nPred, misspecify,
   return(final)
 }
 
-predict.y.en(sampID = 1, nCal = 250, nPred = 250, misspecify = F)
+# predict.y.en(sampID = 1, nCal = 250, nPred = 250, misspecify = F)
 
 #----
