5214b47493a28be1af0faf9fdeb69bcc6261781e
ambhangale <148333008+ambhangale@users.noreply.github.com>
2025-05-08 13:58:29 +0200

    code to run simulation in parallel

---
+++ sim_code/runsim_relaxSEM.R
@@ -0,0 +1,26 @@
+## Aditi M. Bhangale
+## Last updated: 8 May 2025
+
+# Creating a function that applies the RDA-like constraints on the SEM prediction rule
+# relaxed SEM
+### runsim
+
+library(here)
+source(here("sim_code", "wrapper_relaxSEM.R"))
+
+conds <- expand.grid(sampID = 1:100, nCal = c(250, 1e3, 1e4),
+                     misspecify = c(F,T))
+
+t0 <- Sys.time()
+resList <- mcmapply(FUN = wrapper.predict.y, sampID = conds$sampID,
+                    nCal = conds$nCal, nPred = 1e4,
+                    misspecify = conds$misspecify, SIMPLIFY = F,
+                    mc.cores = detectCores() - 1L) 
+# parallelised version of mapply (parallel package). parallel should already be
+# loaded, since it's a dependency for portableParallelSeeds used in the datagen file
+t1 <- Sys.time()
+diff <- difftime(t1, t0, "hour")
+
+saveRDS(resList, file = paste0("PD_resList_", Sys.Date(), ".rds"))
+
+resList <- readRDS("PD_resList_2025-05-08.rds")
