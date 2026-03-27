c7d6f52bb06b9a4663ca1d6a33584373eb43e4db
ambhangale <148333008+ambhangale@users.noreply.github.com>
2026-03-27 16:38:24 +0100

    update SRMR.yx call

--- sim_code/lavcv_relaxSEM.R
+++ sim_code/lavcv_relaxSEM.R
@@ -113,7 +113,7 @@ lav.predict.y <- function(preddat, califit,
     fullSRMR  <- SRMR(S = S, sigma.hat = Sigma)
     xxSRMR    <- SRMR(S = S_xx, sigma.hat = Sigma_xx)
     yySRMR    <- SRMR(S = S_yy, sigma.hat = Sigma_yy)
-    yxSRMR    <- SRMR.yx(S = S, Syx = S_yx, sigmayx.hat = Sigma_yx, 
+    yxSRMR    <- SRMR.yx(S = S, Syx = t(S_xy), sigmayx.hat = t(Sigma_xy), 
                          ynames = ynames, xnames = xnames)
   } else {
     fullSRMR <- xxSRMR <- yySRMR <- yxSRMR <- NULL
